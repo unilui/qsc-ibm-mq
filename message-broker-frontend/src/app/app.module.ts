4b583b3cd4ae6148d377a39b4fa618360a6cabc7
Felipe Camargo <lfelipe_camargo@hotmail.com>
2024-09-24 02:31:10 -0300

    chore: containerize frontend and configure Nginx
    - Dockerize the Angular frontend application
    - Set up Nginx for serving the frontend in the container

--- message-broker-frontend/src/app/app.module.ts
+++ message-broker-frontend/src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
-import { environment } from '../environments/environment';
+import { environment } from '../environment/environment';
 import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
 import { getAuth, provideAuth } from '@angular/fire/auth';
 
