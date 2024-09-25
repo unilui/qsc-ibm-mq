8cfcb8e34cc3e49326fc78dbb8d6e804cfb2bfb1
Felipe Camargo <lfelipe_camargo@hotmail.com>
2024-09-24 21:41:55 -0300

    feat: add basic structure for Spring Boot API project
    - Set up the initial structure for a Spring Boot API
    - Include default configurations and dependencies

---
+++ message-broker-api/src/main/java/com/unilui/message_broker_api/MessageBrokerApiApplication.java
@@ -0,0 +1,13 @@
+package com.unilui.message_broker_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MessageBrokerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MessageBrokerApiApplication.class, args);
+	}
+
+}
---
+++ message-broker-api/src/test/java/com/unilui/message_broker_api/MessageBrokerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unilui.message_broker_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageBrokerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
